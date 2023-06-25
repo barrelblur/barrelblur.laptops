97ac4b4089b8fb6906b332392c8bfc5de5aa57bc
Barrelblur <barreblur@gmail.com>
2023-06-25 22:34:14 +0800

    Date handlers for laptops table

--- lib/tables/laptoptable.php
+++ lib/tables/laptoptable.php
@@ -4,10 +4,13 @@ namespace Barrelblur\Laptops\Tables;
 
 use Barrelblur\Laptops\Contracts\Seedable;
 use Bitrix\Main\Localization\Loc;
-use Bitrix\Main\ORM\Fields\DatetimeField;
+use Bitrix\Main\ORM\Event;
+use Bitrix\Main\ORM\EventResult;
+use Bitrix\Main\ORM\Fields\DateField;
 use Bitrix\Main\ORM\Fields\IntegerField;
 use Bitrix\Main\ORM\Fields\StringField;
 use Bitrix\Main\ORM\Fields\Validators\UniqueValidator;
+use Bitrix\Main\Type\Date;
 
 
 class LaptopTable extends AbstractDataManager implements Seedable
@@ -34,9 +37,29 @@ class LaptopTable extends AbstractDataManager implements Seedable
             new StringField('NAME', ['required' => true]),
             new IntegerField('MODEL_ID', ['required' => true]),
             new IntegerField('BRAND_ID', ['required' => true]),
-            new DatetimeField('AT_ANNOUNCED', ['required' => true]),
+            new DateField('AT_ANNOUNCED', ['required' => true]),
             new IntegerField('PRICE', ['required' => true]),
         ];
+    }
+
+    public static function onBeforeAdd(Event $event): EventResult
+    {
+        return self::handleAtAnnouncedField($event);
+    }
+
+    public static function onBeforeUpdate(Event $event): EventResult
+    {
+        return self::handleAtAnnouncedField($event);
+    }
+
+    public static function handleAtAnnouncedField(Event $event): EventResult
+    {
+        $fields = $event->getParameter('fields');
+
+        $result = new EventResult();
+        $result->modifyFields(['AT_ANNOUNCED' => new Date($fields['AT_ANNOUNCED'], 'Y-m-d')]);
+
+        return $result;
     }
 
     public static function seed(array $resource): void
